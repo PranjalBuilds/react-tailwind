c61e92718b40fa0c983147e912071a60d6d631d8
PranjalBuilds <imcae22060@gmail.com>
2024-10-11 14:12:07 +0530

    static componets (1)

---
+++ src/components/Demo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Demo = () => {
+  return (
+    <div className='border-2 border-solid border-gray-300 p-3 mt-3 ms-2 rounded-md w-90'>
+
+        <img className='w-80 rounded-md h-48' src="/images/temp.jpg"  />
+        <h3 className='font-bold mt-3'>This is a component</h3>
+        <p> This is temporary text. </p>
+    </div>
+  )
+}
+
+export default Demo;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Demo from './components/Demo'
 import App from './App';
+
 
 let name = 'Pranjal';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
     <>
-      <h1 className='font-body text-4xl mt-2 ms-3 font-semibold text-center'>Hello World!</h1>
-      
-      <p className='font-body font-light bg-red-100 text-center '>calculation: {Math.floor(Math.random()*10)} </p>
-      
-      <p className='font-body font-light mt-2 bg-red-100 text-center '>variable check: {name} </p>
+      <h1 className='text-4xl mt-2 ms-3 font-semibold text-center'>Components</h1>
 
+      <div className='flex gap-5 mt-5 justify-center'>
+        <Demo />
+        <Demo />
+        <Demo />
+      </div>
+
+      {/* These are the static compontents, can be used when we have to display same data or sections */}
+  
     </>
 );
 
