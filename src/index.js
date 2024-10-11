111e0968f1254f43e7b1c1f8e70d55689fad00e0
PranjalBuilds <imcae22060@gmail.com>
2024-10-11 23:09:24 +0530

    components by conditional rendering and mapping

--- src/index.js
+++ src/index.js
@@ -1,26 +1,88 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Demo from './components/Demo'
+import FoodItem from './components/Demo'
+
+const foodItems = [
+  {
+    title: "Paneer Tikka",
+    desc: "Grilled marinated paneer cubes with spices.",
+    ratings: 4.5,
+    price: 250,
+    image: "paneer-tikka",
+    votes: 120
+  },
+  {
+    title: "Palak Paneer",
+    desc: "Creamy spinach curry with paneer cubes.",
+    ratings: 4.8,
+    price: 300,
+    image: "palak-paneer",
+    votes: 200
+  },
+  {
+    title: "Chole Bhature",
+    desc: "Spicy chickpeas served with deep-fried bread.",
+    ratings: 4.3,
+    price: 150,
+    image: "chole-bhature",
+    votes: 150
+  },
+  {
+    title: "Aloo Gobi",
+    desc: "Spiced potatoes and cauliflower curry with a delicious flavour.",
+    ratings: 4.2,
+    price: 180,
+    image: "aloo-gobi",
+    votes: 100
+  },
+  {
+    title: "Dal Makhani",
+    desc: "Rich lentil dish cooked with butter and cream.",
+    ratings: 4.7,
+    price: 220,
+    image: "dal-makhani",
+    votes: 180
+  },
+  {
+    title: "Vegetable Biryani",
+    desc: "Fragrant basmati rice cooked with mixed vegetables.",
+    ratings: 4.6,
+    price: 350,
+    image: "vegetable-biryani",
+    votes: 220
+  },
+  {
+    title: "Masoor Dal",
+    desc: "Red lentils cooked with spices and herbs.",
+    ratings: 4.4,
+    price: 160,
+    image: "masoor-dal",
+    votes: 90
+  },
+  {
+    title: "Baingan Bharta",
+    desc: "Smoky roasted eggplant mashed and spiced.",
+    ratings: 4.5,
+    price: 200,
+    image: "baingan-bharta",
+    votes: 110
+  }
+];
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
     <>
-      <h1 className='text-4xl mt-3 ms-3 font-semibold text-center'>Components</h1>
+      <div className='flex flex-wrap place-content-evenly'>
 
-      <div className='flex gap-5 mt-5 justify-center'>
-        <Demo title="Burger" desc="Juicy, big, loaded with toppings of your choice" price="₹ 200"/>
-        <Demo title="Pizza" desc="Cheesy pizza with pepperoni toppings." price="₹ 450"/>
-        <Demo title="Sushi" desc="Fresh sushi rolls with salmon and avocado." price="₹ 600"/>
-        <Demo title="Pasta" desc="Creamy Alfredo pasta with juice." price="₹ 350"/>
-        <Demo title="Ice-Cream" desc="Vanilla ice cream with chocolate syrup." price="₹ 120"/>
-        <Demo title="Salad" desc="Healthy green salad with a variety of veggies." price="₹ 180"/>
+      {
+        foodItems.map((item) => {
+          return <FoodItem title={item.title} ratings={item.ratings} desc={item.desc} img={item.image} price={item.price} />
+        })
+      }
       </div>
-
-
-      {/* These are the static compontents, can be used when we have to display same data or sections */}
-  
     </>
 );
 
