de1754c18b30b2834031248e613a8525ebb384ae
PranjalBuilds <imcae22060@gmail.com>
2024-10-11 21:04:22 +0530

    components and props

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,12 +1,22 @@
 import React from 'react'
 
-const Demo = () => {
+const Demo = (props) => {
   return (
     <div className='border-2 border-solid border-gray-300 p-3 mt-3 ms-2 rounded-md w-90'>
 
-        <img className='w-80 rounded-md h-48' src="/images/temp.jpg"  />
-        <h3 className='font-bold mt-3'>This is a component</h3>
-        <p> This is temporary text. </p>
+        <img className='w-80 rounded-md h-48' src={`https://dummyimage.com/1600x900/000/fff&text=${props.title}`} alt={props.title} /> 
+
+        <div className='flex place-content-between mb-2'>
+          <h3 className='font-bold mt-3'>{props.title}</h3>
+          <h3 className='bg-red-600 h-6 mt-3 ps-2 pe-2 text-white rounded-md'> {props.price} </h3>
+        </div>
+        <p> {props.desc} </p>
+        
+        <div className='flex place-content-between'>
+          <button className='bg-red-600 text-xl h-10 w-70 mt-3 ps-8 pe-8 text-white rounded-md'>Buy now</button>
+          <button className='bg-yellow-400 text-xl mt-3 ps-3 pe-3 rounded-md'> <img src="/images/add-to-cart.svg" alt="add to cart" /></button>
+        </div>
+
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,21 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Demo from './components/Demo'
-import App from './App';
 
-
-let name = 'Pranjal';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
     <>
-      <h1 className='text-4xl mt-2 ms-3 font-semibold text-center'>Components</h1>
+      <h1 className='text-4xl mt-3 ms-3 font-semibold text-center'>Components</h1>
 
       <div className='flex gap-5 mt-5 justify-center'>
-        <Demo />
-        <Demo />
-        <Demo />
+        <Demo title="Burger" desc="Juicy, big, loaded with toppings of your choice" price="₹ 200"/>
+        <Demo title="Pizza" desc="Cheesy pizza with pepperoni toppings." price="₹ 450"/>
+        <Demo title="Sushi" desc="Fresh sushi rolls with salmon and avocado." price="₹ 600"/>
+        <Demo title="Pasta" desc="Creamy Alfredo pasta with juice." price="₹ 350"/>
+        <Demo title="Ice-Cream" desc="Vanilla ice cream with chocolate syrup." price="₹ 120"/>
+        <Demo title="Salad" desc="Healthy green salad with a variety of veggies." price="₹ 180"/>
       </div>
+
 
       {/* These are the static compontents, can be used when we have to display same data or sections */}
   
