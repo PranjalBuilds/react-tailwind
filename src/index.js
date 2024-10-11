f63b96a6151cbecaac3942e077a665ac4c0004b6
PranjalBuilds <imcae22060@gmail.com>
2024-10-11 12:54:43 +0530

    Hello world

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  
+    <>
+      <h1 className='font-body text-4xl mt-2 ms-3 font-semibold'>Hello World!</h1>
+      <p className='font-body ms-3 font-light bg-red-100 inline'>Welcome to the react project with tailwindcss!</p>
+    </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
