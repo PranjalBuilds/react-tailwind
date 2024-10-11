ce6a3230febac9aed06ee72ef56512bb0631ac62
PranjalBuilds <imcae22060@gmail.com>
2024-10-11 13:24:50 +0530

    Using JS in React

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-
+let name = 'Pranjal';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
     <>
-      <h1 className='font-body text-4xl mt-2 ms-3 font-semibold'>Hello World!</h1>
-      <p className='font-body ms-3 font-light bg-red-100 inline'>Welcome to the react project with tailwindcss!</p>
+      <h1 className='font-body text-4xl mt-2 ms-3 font-semibold text-center'>Hello World!</h1>
+      
+      <p className='font-body font-light bg-red-100 text-center '>calculation: {Math.floor(Math.random()*10)} </p>
+      
+      <p className='font-body font-light mt-2 bg-red-100 text-center '>variable check: {name} </p>
+
     </>
 );
 
