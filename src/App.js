4f74d9ff507875d8f9ad891b3e23f1187f10adbd
PranjalBuilds <imcae22060@gmail.com>
2024-10-14 21:59:26 +0530

    traditional way to update values in JS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-function App() {
+import React from 'react'
+
+let counter = 0;
+
+const App = () => {
+
+  let incrementHandler = () => {
+    console.log(counter+=1);
+  }
   return (
-    <App />
-  );
+    <>
+      <h1 className='text-center my-4 font-bold text-3xl'> React useState hook </h1>
+      <h1 className='counter text-4xl text-center '>{counter}</h1>
+      
+      <div className='text-center'>
+        <button className='py-3 px-8 my-4 text-3xl bg-blue-600 active:bg-blue-500 text-white rounded-lg' onClick={incrementHandler}>Increment + </button>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
